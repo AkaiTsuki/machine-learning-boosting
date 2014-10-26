3475f91abeba3daf1b26ab9a5cae8543b863ffd7
Jiachi Liu <liu.jiach@husky.neu.edu>
2014-10-25 20:45:39 -0400

    Added code to compare performance

--- boosting/ActiveLearning.py
+++ boosting/ActiveLearning.py
@@ -8,21 +8,24 @@ class ActiveLearning:
     def __init__(self):
         # save the index of data point that already selected
         self.selected = {}
+        self.result = []
 
     def active_learning(self, train, train_target, test, test_target):
-        init_size = int(len(train) * 0.05)
-        increment_size = int(len(train) * 0.02)
-        upper_size = int(len(train) * 0.5)
+        param = 0.05
+        increment = 0.05
+        init_size = int(len(train) * param)
+        increment_size = int(len(train) * increment)
 
         X = train[:init_size]
         Y = train_target[:init_size]
         R = train[init_size:]
         RY = train_target[init_size:]
 
-        while len(X) < upper_size:
+        while param < 0.5:
             print "labeled data: %.2f%%" % (100.0 * len(X)/len(train))
             adaboost = AdaBoost(OptimalWeakLearner())
-            adaboost.boost(X, Y, test, test_target)
+            acc, err, auc = adaboost.boost(X, Y, test, test_target)
+            self.result.append((acc, err, auc))
             H = adaboost.hypothesis(R)
             H_abs = np.abs(H)
             sorted_indices = H_abs.argsort().tolist()
@@ -34,5 +37,6 @@ class ActiveLearning:
             Y = np.append(Y, RY[selected])
             R = R[remained]
             RY = RY[remained]
+            param += increment
 
 
--- main_active_learning.py
+++ main_active_learning.py
@@ -10,3 +10,4 @@ train, target = load_spambase()
 target = np.array(map(lambda v: -1.0 if v == 0 else 1.0, target))
 train, test, train_target, test_target = train_test_shuffle_split(train, target, len(train) / 10)
 al.active_learning(train, train_target, test, test_target)
+print al.result
